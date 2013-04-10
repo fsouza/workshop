82ff71b30e234806662c8e026dc8e93f7618ce7f
Francisco Souza <f@souza.cc>
2013-04-09 21:04:00 -0300

    basic: added very first example

---
+++ basic/hello.go
@@ -0,0 +1,11 @@
+// Copyright 2013 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
