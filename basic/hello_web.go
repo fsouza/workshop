0dfdd290d585dca2d06a5f09e1f1c783070f05a5
Francisco Souza <f@souza.cc>
2013-04-09 21:04:10 -0300

    basic: add web server version of hello world

---
+++ basic/hello_web.go
@@ -0,0 +1,17 @@
+// Copyright 2013 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello world!")
+	})
+	http.ListenAndServe(":6060", nil)
+}
