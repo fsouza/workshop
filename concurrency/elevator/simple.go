be06861cd0b0a550a5a99283ae1fd5cd55867ba6
Francisco Souza <f@souza.cc>
2013-04-13 19:34:07 -0300

    example: add simpler example

---
+++ concurrency/elevator/simple.go
@@ -0,0 +1,38 @@
+// Copyright 2013 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	Name string
+}
+
+func transport() chan<- Person {
+	people := make(chan Person, 2)
+	go func() {
+		for p := range people {
+			fmt.Printf("Transporting %q...\n", p.Name)
+			time.Sleep(1e9)
+		}
+	}()
+	return people
+}
+
+func main() {
+	people := []Person{
+		{Name: "Bob"}, {Name: "Mary"}, {Name: "Thomas"},
+		{Name: "John"}, {Name: "Peter"}, {Name: "Ken"},
+		{Name: "Patricia"}, {Name: "Ane"}, {Name: "Alice"},
+	}
+	ch := transport()
+	for _, person := range people {
+		ch <- person
+	}
+	close(ch)
+}
