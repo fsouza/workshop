ab3c2eefdb8676207b1242260e7ab1d0c4fda462
Francisco Souza <f@souza.cc>
2013-04-14 17:36:07 -0300

    basic: added vars example

---
+++ basic/vars.go
@@ -0,0 +1,12 @@
+// Copyright 2013 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	var i int
+	var j int = 10
+	var k = 10
+	l := 10
+}
