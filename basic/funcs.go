e00459e452a72f736bc0a2fb1b925a3f0fc1403a
Francisco Souza <f@souza.cc>
2013-04-14 17:59:44 -0300

    basic/funcs: add multiple return functions and named results

--- basic/funcs.go
+++ basic/funcs.go
@@ -14,6 +14,17 @@ func apply(values []int, fn func(int)) {
 	}
 }
 
+// count people in the database
+func countPeople() (int, error) {
+	n := 10
+	return n, nil
+}
+
+func getInfo() (result int, err error) {
+	result = 10
+	return
+}
+
 func main() {
 	values := []int{1, 2, 3}
 	apply(values, func(i int) { fmt.Println(i) })
