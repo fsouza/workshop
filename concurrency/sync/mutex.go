b732afb0b0c02a479e88c87adaa5e7617ccc6128
Francisco Souza <f@souza.cc>
2013-04-14 00:41:06 -0300

    concurrency/sync: added Mutex example

---
+++ concurrency/sync/mutex.go
@@ -0,0 +1,24 @@
+// Copyright 2013 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"sync"
+)
+
+var hits struct {
+	m map[string]int
+	sync.Mutex
+}
+
+func hit(name string) {
+	hits.Lock()
+	hits.m[name] += 1
+	hits.Unlock()
+}
+
+func main() {
+
+}
