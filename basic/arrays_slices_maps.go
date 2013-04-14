7614db7f21a157e158af7ed75dea5978a45797f9
Francisco Souza <f@souza.cc>
2013-04-14 17:44:06 -0300

    basic: add for examples to arrays_slices_maps

--- basic/arrays_slices_maps.go
+++ basic/arrays_slices_maps.go
@@ -4,17 +4,34 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	var values []int
 	values = []int{10, 15, 18}
 	values = values[:2]
+	for i := 0; i < len(values); i++ {
+		fmt.Printf("values[%d]: %d.\n", i, values[i])
+	}
+
+	for i, value := range values {
+		fmt.Printf("values[%d]: %d.\n", i, value)
+	}
 
 	var array [16]int
 	values = array[:]
+	for i, value := range array {
+		fmt.Printf("array[%d]: %d.\n", i, value)
+	}
 
 	var count map[string]int
 	count = make(map[string]int)
 	count["x"] = 1
 	count["y"] = 1
 	count["x"] += 1
+	for key, value := range count {
+		fmt.Printf("count[%q]: %d.\n", key, value)
+	}
 }
